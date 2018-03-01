3f68eb1172be1113c9c187aefb1b9de3db57bd71
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-01 16:50:08 +0530

    Update noof_words.c

--- noof_words.c
+++ noof_words.c
@@ -1,16 +1,23 @@
-#include<stdio.h>
-void main()
+
+
+#include <stdio.h>
+#include<string.h>
+int main()
 {
-char str[30];
+  char str[30];
 int count,i;
 printf("enter the string");
-scanf("%s",&str);
+scanf("%[^\n]%*c",&str);
 for(i=0;str[i]!='\0';i++)
 {
-  if(str[i]==' ')
+  if(str[i]==32)
   {
 count++;
   }
 }
 printf("number of words in a sentence is %d",++count);
+    return 0;
 }
+
+
+
