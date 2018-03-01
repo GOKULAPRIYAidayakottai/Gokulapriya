0198b617906b31983974e97154c4e6bcd59e4a90
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-01 15:38:43 +0530

    Update perf_squ.c

--- perf_squ.c
+++ perf_squ.c
@@ -1,17 +1,20 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+#include<math.h>
+int main()
 {
-int n1,n2,i;
-pritntf("enter the numbers");
-scanf("%d%d",&n,&n2);
+    int n1,n2,i,pro;
+printf("enter the numbers");
+scanf("%d%d",&n1,&n2);
 pro=n1*n2;
 for(i=0;i<(pro/2);i++)
 {
-if((pow(i,2)==pro)
+if(pow(i,2)==pro)
 {
 printf("product is perfect square");
 break;
 }
 }
+
+    return 0;
 }
 
