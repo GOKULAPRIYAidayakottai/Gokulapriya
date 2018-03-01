7c6da36742eedaa498a7f188eae6acfa4f826a39
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-01 16:13:50 +0530

    Update factors.c

--- factors.c
+++ factors.c
@@ -1,16 +1,17 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+#include<math.h>
+int main()
 {
-int n,i;
+   int n,i;
 printf("enter the number");
 scanf("%d",&n);
-printf("the factors are");
+printf("the factors are  ");
 for(i=1;i<=n;i++)
 {
 if((n%i)==0)
 {
-printf("%d",i);
+printf("%d\t",i);
 }
 }
+    return 0;
 }
-
