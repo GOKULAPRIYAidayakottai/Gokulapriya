ec123d721ce79c97a3846829f602459f124a58aa
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-01 16:21:19 +0530

    Update Nbetween.c

--- Nbetween.c
+++ Nbetween.c
@@ -4,7 +4,7 @@ void main()
 int n,l,r;
 printf("enter the number");
 scanf("%d",&n);
-printf(enter the left and right limit");
+printf("enter the left and right limit");
 scanf("%d%d",&l,&r);
 if((n>l)&&(n<r))
 {
