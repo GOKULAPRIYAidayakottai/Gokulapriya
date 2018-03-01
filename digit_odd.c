043650f25116c560502ec0396d88105880630426
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-01 16:11:49 +0530

    Update digit_odd.c

--- digit_odd.c
+++ digit_odd.c
@@ -1,17 +1,29 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+#include<math.h>
+int main()
 {
-int a[20],i=0;
-printf("enter the digit");
-scanf("%s",&a);
-while(isdigit(a[i]))
-{
-if((a[i]%2)!=0)
-{
-printf("%d\t",a[i]);
-}
-i++;
-}
+    int n,r,i,k=0,a[20];
+    printf("enter the number:");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        
+        r=n%10;
+       
+        if((r%2)!=0)
+        {
+            a[k]=r;
+            k++;
+        }
+        n=n/10;
+    }
+    
+    for(i=k-1;i>=0;i--)
+    {
+        printf("%d\t",a[i]);
+    }
+    
+    return 0;
 }
 
 
