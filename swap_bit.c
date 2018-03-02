113a0c210d2ca95bbb7bc24f92dbdacb38a31762
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-02 14:19:56 +0530

    Update swap_bit.c

--- swap_bit.c
+++ swap_bit.c
@@ -7,6 +7,6 @@ void main()
     i = i ^ k;
     k = i ^ k;
     i = i ^ k;
-    printf("value of i=%d k=%d after swapping", i, k);
+    printf("\nvalue of i=%d k=%d after swapping", i, k);
     
  }
