bb5602ef83eafb8cf914082131da10e3f444e4d0
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-02 15:15:02 +0530

    Update sub_time.c

--- sub_time.c
+++ sub_time.c
@@ -1,5 +1,9 @@
-#include<stdio.h>
-void main()
+
+
+#include <stdio.h>
+#include<string.h>
+
+int main()
 {
 int t1,t2,t3,min1,min2,min3,hr1,hr2,hr3;
 printf("enter the first time in hr and min");
@@ -11,6 +15,9 @@ t2=(hr2*60)+min2;
 t3=t1-t2;
 hr3=t3/60;
 min3=t3%60;
-printf("difference between two times are  %d:%d",hr3,min3);
+printf("difference between two times are  %d:%d",abs(hr3),abs(min3));
+    return 0;
 }
+
+
 
