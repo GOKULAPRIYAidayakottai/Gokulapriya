cab75caacc139843fb675cdab40e7699392bfee8
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-02 14:45:40 +0530

    Update strcmp.c

--- strcmp.c
+++ strcmp.c
@@ -1,13 +1,16 @@
-#include<stdio.h>
-void main()
+
+
+#include <stdio.h>
+#include<string.h>
+int main()
 {
-char str1[20],str2[20];
-int i=0,count=0;
-  ptintf("enter the the 1st string");
-  scanf("%s",&str1);
-   ptintf("enter the the 2nd string");
+    char str1[20],str2[20];
+int i=0,count=0,l=0;
+  printf("enter the the 1st string");
+ scanf("%s",str1);
+   printf("enter the the 2nd string");
   scanf("%s",&str2);
-  
+  l=strlen(str1);  
 while((str1[i]!='\0')||(str2[i]!='\0'))
 {
 if(str1[i]==str2[i])
@@ -26,8 +29,12 @@ printf("greatest string is %s",str2);
 break;
 }
 }
-if(count!=0)
+if(count==l)
 {
 printf("strings are same.....the string is %s",str1);
 }
+    return 0;
 }
+
+
+
