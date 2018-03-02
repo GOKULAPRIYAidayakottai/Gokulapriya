951541a63c97bc19606fca0ae9f68589d532f0b4
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-02 15:18:14 +0530

    Update ele_index.c

--- ele_index.c
+++ ele_index.c
@@ -1,5 +1,8 @@
-#include<stdio.h>
-void main()
+
+#include <stdio.h>
+#include<string.h>
+
+int main()
 {
 int n,arr[1000],i=0;
 printf("enter array limit");
@@ -7,11 +10,15 @@ scanf("%d",&n);
 printf("enter array elements");
 for(i=0;i<n;i++)
 {
-scanf("%d",&arr[i])
+scanf("%d",&arr[i]);
 }
 printf("array element with it's index");
 for(i=0;i<n;i++)
 {
-printf("%d\t%d",arr[i],i);
+printf(" %d %d ",arr[i],i);
 }
+    return 0;
 }
+
+
+
