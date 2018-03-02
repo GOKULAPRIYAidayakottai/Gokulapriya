d5b9df8ad9db9219873003300e28322791b35656
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-02 15:08:31 +0530

    Update count_space.c

--- count_space.c
+++ count_space.c
@@ -1,17 +1,25 @@
-#include<stdio.h>
-void main()
+
+
+#include <stdio.h>
+#include<string.h>
+
+int main()
 {
 char str[30];
 int i=0,count=0;
   printf("enter the string");
-  scanf("%s",&str);
+  scanf("%[^\n]s",&str);
 while(str[i]!='\0')
 {
-if(str[i]==" ")
+if(str[i]==32)
 {
 ++count;
 }
 ++i;
 }
 printf("no of space is %d ",count);
+    return 0;
 }
+
+
+
