2b28f641d0e40dc8e7ec264e848fde1488971bb0
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-02 14:17:09 +0530

    Update fibo2.c

--- fibo2.c
+++ fibo2.c
@@ -1,15 +1,23 @@
-#include<stdio.h>
-void main()
+
+
+#include <stdio.h>
+#include<string.h>
+int main()
 {
-int f=0,s=1,N,i,temp;
+    int f=0,s=1,N,i,temp=0;
 scanf("%d",&N);
 printf("FIBONACCI SERIES....");
-printf("%d%d",f,s);
+printf("%d %d ",f,s);
 for(i=2;i<=N;i++)
 {
 temp=f+s;
-f=f+s;
-s=s+temp;
-printf("%d",temp);
+printf("%d ",temp);
+f=s;
+s=temp;
+
 }
+    return 0;
 }
+
+
+
