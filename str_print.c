0c66fe65546d4192174e6a3938f79ba439c5d4df
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-02 14:46:57 +0530

    Update str_print.c

--- str_print.c
+++ str_print.c
@@ -1,14 +1,21 @@
-#include<stdio.h>
-void main()
+
+
+#include <stdio.h>
+#include<string.h>
+int main()
 {
-char str[30];
+    char str[30];
 int n,i;
 printf("enter the string");
 scanf("%s",&str);
-printf("enter the no of times to print hte string");
+printf("enter the no of times to print the string");
   scanf("%d",&n);
   for(i=0;i<n;i++)
   {
-    printf("%s",str);
+    printf("%s\n",str);
   }
+    return 0;
 }
+
+
+
