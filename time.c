a7474b634b226c07e437e59c605a9e21bb0e2edd
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-02 15:16:45 +0530

    Update time.c

--- time.c
+++ time.c
@@ -1,10 +1,18 @@
-#include<stdio.h>
-void main()
+
+
+#include <stdio.h>
+#include<string.h>
+
+int main()
 {
 int m,hr,min;
 printf("enter time in minute");
 scanf("%d",&m);
-hr=m/52;
-min=m%52;
+hr=m/60;
+min=m%60;
 printf("%d:%d",hr,min);
+    return 0;
 }
+
+
+
