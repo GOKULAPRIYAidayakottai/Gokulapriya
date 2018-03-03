765ed36ea646ad657317dac73d5cfd746fdec522
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-03 16:44:55 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,9 +1,13 @@
-#include<stdio.h>
-void main()
+
+
+#include <stdio.h>
+
+int main()
 {
-int n ,i,count=0;
+   
+    int n ,i,count=0;
 scanf("%d",&n);
-for(i=1;i<=n;i++)
+for(i=2;i<n;i++)
 {
 if((n%i)==0)
 {
@@ -18,4 +22,7 @@ else
 {
 printf("not a prime number");
 }
+return 0;
 }
+    
+
