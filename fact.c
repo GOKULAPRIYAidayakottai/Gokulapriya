a29e0f846930ce5b0abe3aac4a21794887ea129e
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-03 16:27:50 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
-void main()
+
+#include <stdio.h>
+
+int main()
 {
-int n,fact=1,i;
-scanf("%d"",n);
-for(i=0;i<=n;i++)
+    int n,fact=1,i;
+scanf("%d",&n);
+for(i=1;i<=n;i++)
 {
 fact=fact*i;
 }
 printf("%d",fact);
+    return 0;
 }
