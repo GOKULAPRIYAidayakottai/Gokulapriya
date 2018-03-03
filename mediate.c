1d2e169ded2dad18326b7f3e2b32c85015795b41
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-03 15:42:08 +0530

    Update mediate.c

--- mediate.c
+++ mediate.c
@@ -1,7 +1,10 @@
-#include<stdio.h>
-void main()
+
+
+#include <stdio.h>
+
+int main()
 {
-int n,arr[1000],i=0,mid;
+    int n,arr[1000],i=0,j,temp,mid;
 printf("enter array limit");
 scanf("%d",&n);
 if(!(n<=1000)&&(n>=1))
@@ -13,9 +16,26 @@ else
 printf("enter array elements");
 for(i=0;i<n;i++)
 {
-scanf("%d",&arr[i])
+scanf("%d",&arr[i]);
+}
+for(j=0;j<n-1;j++)
+    {
+for(i=j;i<n;i++)
+{
+if(arr[i]>arr[j])
+{
+temp=arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+}
 }
 mid=n/2;
 printf("middle element is %d",arr[mid]);
 }
+    
+    return 0;
 }
+
+
+
