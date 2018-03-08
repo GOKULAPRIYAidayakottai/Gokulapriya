e4ab745888fc8411c0672fbb57c16b170ba374ac
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-08 16:42:43 +0530

    Update Nchange.c

--- Nchange.c
+++ Nchange.c
@@ -1,16 +1,37 @@
-#include<stdio.h>
-void main()
+
+#include <stdio.h>
+#include<math.h>
+int main()
 {
-int n[20],i=0,N;
-printf("enter the numbers");
-scanf("%s",&n);
-printf("enter N value");
+  int a[20],i=0,N,j,t,c=0;
+printf("enter the number of elements");
 scanf("%d",&N);
+printf("enter array values");
 for(i=0;i<N;i++)
 {
-if(a[i]!=i)
+    scanf("%d",&a[i]);
+}
+for(i=0;i<N-1;i++)
 {
-printf("%d",i);
+    for(j=i;j<N;j++)
+    {
+        if(a[i]>a[j])
+        {
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+            c++;
+            if(c==1)
+        {
+            printf("%d",j);
+        }
+        }
+        
+    }
+
 }
+    return 0;
 }
-}
+
+
+
