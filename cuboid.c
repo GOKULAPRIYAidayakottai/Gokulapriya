15fe13a3b0fd39f9c0624dc8cc3c5c93bd7d75b0
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-08 16:25:07 +0530

    Update cuboid.c

--- cuboid.c
+++ cuboid.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void main()
 {
-int i,w,h,sa,vol;
+int l,w,h,sa,vol;
 printf("enter l,w,h:");
 scanf("%d%d%d",&l,&w,&h);
 vol=l*w*h;
 sa=(2*l*w)+(2*l*h)+(2*h*w);
 printf("surface area of cuboid = %d",sa);
-printf("volume of cuboid = %d",vol);
+printf("\nvolume of cuboid = %d",vol);
 }
