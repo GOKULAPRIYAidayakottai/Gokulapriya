111615b997de61863222796095fab62147234c84
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-08 16:27:42 +0530

    Update temperature.c

--- temperature.c
+++ temperature.c
@@ -1,9 +1,17 @@
-#include<stdio.h>
-void main()
+
+
+#include <stdio.h>
+#include<math.h>
+int main()
 {
-float kel,cel;
+    float kel,cel;
 printf("enter temperatue in celcius");
 scanf("%f",&cel);
 kel=cel+273.15;
-printf("kelvin = %f"kel);
+printf("kelvin = %.0f",kel);
+
+    return 0;
 }
+
+
+
