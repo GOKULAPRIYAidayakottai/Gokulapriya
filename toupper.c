664423a0c8b9d6ef0dfa1373ae1219d1daa2ddd0
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-08 15:58:02 +0530

    Update toupper.c

--- toupper.c
+++ toupper.c
@@ -1,19 +1,24 @@
-#include<iostream.h>
+
+#include <stdio.h>
 #include<string.h>
-void main()
+int main()
 {
-char ch[30];
-int l;
-printf("enter the string");
-scanf("%s",&ch);
+    char ch[30];
+int l,i;
+printf("enter the string ");
+scanf("%[^\n]s",&ch);
 l=strlen(ch);
 ch[0]=toupper(ch[0]);
 for(i=1;i<l;i++)
 {
-if(ch[i]==" ")
+if(ch[i]==32)
 {
 ch[i+1]=toupper(ch[i+1]);
 }
 }
 printf("the string is = %s",ch);
+
+    return 0;
 }
+
+
