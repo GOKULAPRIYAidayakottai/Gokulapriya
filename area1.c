a27fbc0c12fcd231b68f52bf9f2f917437ce9e10
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-08 16:48:30 +0530

    Update area1.c

--- area1.c
+++ area1.c
@@ -1,8 +1,14 @@
-#include<stdio.h>
-void main()
+
+#include <stdio.h>
+#include<math.h>
+int main()
 {
-float b,l,area;
-printf("enter length and breadth);
+ float b,l,area;
+printf("enter length and breadth");
 scanf("%f%f",&l,&b);
 printf("area = %f",(l*b));
+    return 0;
 }
+
+
+
